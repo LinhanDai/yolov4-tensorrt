162f315d0fcd734b015f15a21477b5cafcfbc81b
LinhanDai <dailinhan9619@163.com>
2022-03-21 11:51:26 +0800

    [yolo-tensorrt] 搭建初步写代码的环境
    1. 搭建环境
    
    Signed-off-by: LinhanDai <dailinhan9619@163.com>

---
+++ include/yolov4.h
@@ -0,0 +1,8 @@
+//
+// Created by linhan on 2022/3/21.
+//
+
+#ifndef YOLO_TRT_YOLOV4_H
+#define YOLO_TRT_YOLOV4_H
+
+#endif //YOLO_TRT_YOLOV4_H
--- src/yolov4.cpp
+++ src/yolov4.cpp
@@ -1,7 +1,7 @@
 //
 // Created by linhan on 2022/3/21.
 //
-
+#include "yolov4.h"
 
 int main()
 {
