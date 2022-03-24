f141d17fa7ac96d49171f2b4c3d6cd916dbb0e0e
dailinhan <dailinhan9619@163.com>
2022-03-24 15:21:54 +0800

    yolov4 tensorrt部署
    1. yolov4 tensorrt部署
    
    Signed-off-by: dailinhan <dailinhan9619@163.com>

--- yolov4_demo.cpp
+++ yolov4_demo.cpp
@@ -3,7 +3,7 @@
 //
 #include "yolov4.h"
 
-void showResult(const std::vector<detectResult>& result, cv::Mat img)
+void showResult(const std::vector<detectResult>& result, std::vector<cv::Mat> &imgCloneBatch)
 {
     std::vector<cv::Scalar> colors;
     colors.emplace_back(255, 0, 0);
@@ -13,36 +13,46 @@ void showResult(const std::vector<detectResult>& result, cv::Mat img)
     colors.emplace_back(255, 0, 255);
     colors.emplace_back(255, 255, 255);
     colors.emplace_back(0, 0, 0);
-    for (const auto& batchResult: result)
+    for (int i = 0; i < result.size(); i++)
     {
+        detectResult batchResult = result[i];
         for (const auto& r: batchResult)
         {
             std::stringstream stream;
             stream << std::fixed << std::setprecision(2) << "id:" << r.id << "  score:" << r.prob;
-            cv::rectangle(img, r.rect, colors[r.id], 2);
-            cv::putText(img, stream.str(), cv::Point(r.rect.x, r.rect.y - 5), 0, 0.5, cv::Scalar(0, 0, 255), 2);
+            cv::rectangle(imgCloneBatch[i], r.rect, colors[r.id], 2);
+            cv::putText(imgCloneBatch[i], stream.str(), cv::Point(r.rect.x, r.rect.y - 5), 0, 0.5, cv::Scalar(0, 0, 255), 2);
         }
+        cv::namedWindow("Windows", cv::WINDOW_NORMAL);
+        cv::resizeWindow("Windows", imgCloneBatch[i].cols / 2, imgCloneBatch[i].rows / 2);
+        cv::imshow("Windows", imgCloneBatch[i]);
+        cv::waitKey(1);
     }
-    cv::namedWindow("Windows", cv::WINDOW_NORMAL);
-    cv::resizeWindow("Windows", img.cols / 2, img.rows / 2);
-    cv::imshow("Windows", img);
-    cv::waitKey(0);
 }
 
 int main()
 {
-    std::vector<cv::Mat> imgBatch;
-    cv::Mat imgClone;
     std::string configPath = "../configs";
-    cv::Mat img = cv::imread("../data/2.jpg", cv::IMREAD_COLOR);
-    imgClone = img.clone();
-    imgBatch.push_back(img);
+    std::vector<cv::String> images;
+    cv::String path("../data/*.jpg");
+    cv::glob(path, images);
     std::shared_ptr<YoloV4> yoloObj = std::make_shared<YoloV4>(configPath);
-//    double start = cv::getTickCount();
-    std::vector<detectResult> result = yoloObj->detect(imgBatch);
-//    double end = cv::getTickCount();
-//    std::cout << "fps:" << 1 / ((end - start) / cv::getTickFrequency()) << std::endl;
-
-    showResult(result, imgClone);
+    for (auto image: images)
+    {
+        std::vector<cv::Mat> imgCloneBatch;
+        std::vector<cv::Mat> batch_img;
+        cv::Mat img = cv::imread(image, cv::IMREAD_COLOR);
+        batch_img.push_back(img);
+        for (int i = 0; i < batch_img.size(); i++)
+        {
+            imgCloneBatch.push_back(batch_img[i].clone());
+        }
+        double start = cv::getTickCount();
+        std::vector<detectResult> result = yoloObj->detect(batch_img);
+        double end = cv::getTickCount();
+        double fps =  1 / ((end - start) / cv::getTickFrequency());
+        std::cout << "fps:" << fps << std::endl;
+        showResult(result, imgCloneBatch);
+    }
     return 0;
 }
